9ca237df39b2f6d8cf77291312beda9595d63f78
Temesgen-lab <153854077+Temesgen-lab@users.noreply.github.com>
2024-11-11 06:46:29 +0000

    header mobile navigation

--- Hotel/src/components/Header/Header.jsx
+++ Hotel/src/components/Header/Header.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React,{useState} from 'react';
 import style from './header.module.css'
 import logo from './img/hotel-logo.png'
+import MenuIcon from '@mui/icons-material/Menu';
+import CloseIcon from '@mui/icons-material/Close';
 
 
 function Header() {
+const [isClicked , setIsClicked]=useState(false);
 
-  return (
+function clickHandler() {
+  setIsClicked(prev => !prev);
+}
+  return (<div>
     <div className={style.header_outer_container}>
       <nav className={style.header_container}>
-
+        <p className={style.menu_icon} onClick={clickHandler}> <MenuIcon /> </p>
         <div className={style.logo_box}>
-          <img src={logo} alt='hotel - logo' className={style.logo} />
+         
         </div>
         <ul className={style.header_nav}>
           <li>Home</li>
@@ -25,6 +31,25 @@ function Header() {
         <button className={style.book_btn}>Book Now</button>
       </nav>
     </div>
+
+    <div className={style.mobile_nav_container} style={{
+      opacity:isClicked?1:0,
+      pointerEvents:isClicked?'auto':'none',
+      visibility:isClicked?'visible':'hidden'
+    }}> 
+
+<CloseIcon className={style.closeIcon} onClick={clickHandler}/>
+    <ul className={style.mobile_nav}>
+          <li>Home</li>
+          <li>About</li>
+          <li>Services</li>
+          <li>Rooms</li>
+          <li>blog</li>
+          <li>events</li>
+          <li>contact</li>
+</ul>
+    </div></div>
+
   )
 }
 
