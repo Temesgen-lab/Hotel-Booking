dba6d59c1274b01f54bfc2563a523ab116d58c16
Temesgen-lab <153854077+Temesgen-lab@users.noreply.github.com>
2024-11-12 20:21:01 +0000

    room

--- Hotel/src/components/Banner/img/data.js
+++ Hotel/src/components/Banner/img/data.js
@@ -2,6 +2,8 @@ import img1 from './banner1.jpg'
 import img2 from './banner2.jpg'
 import img3 from './banner3.jpg'
 import img4 from './banner4.jpg'
+import img5 from './banner5.jpg'
+import img6 from './banner6.jpg'
 
-const data=[img1,img2,img3,img4];
+const data=[img1,img2,img3,img4,img5,img6];
 export default data;
