70b11500a1e4d1f30512d544be504d5fe47e7016
Temesgen-lab <153854077+Temesgen-lab@users.noreply.github.com>
2024-11-12 04:40:48 +0000

    banner content

--- Hotel/src/components/Banner/Banner.jsx
+++ Hotel/src/components/Banner/Banner.jsx
@@ -7,16 +7,43 @@ import data from './img/data'
 function Banner() {
     return (
         <>
+       <div className={style.overlay}></div>
     <Carousel
-    autoPlay={true}
-    infiniteLoop={true}
-   interval={2000}
-   transitionTime={1000}
+    autoPlay
+    infiniteLoop
+    interval={3000} // Change to 3000ms or higher
+    // transitionTime={1000} // Optional: 1 second transition
     showStatus={false}
     showThumbs={false}
     showIndicators={false}
+   
     >
-       { data.map(img => <img src={img} alt="image" className={style.carousel}/>)}
+       { data.map((img,index)=> <div className={style.carousel_box} key={index}>
+        <div className={style.banner_content}>
+         <p className={style.hero_desc}>make yourself at home in our hotel</p>
+               <div className={style.banner_form}>
+                <input type="text"  placeholder='check in' className={style.check_input}/>
+                <input type="text" placeholder='check out' />
+                <select name="" id="" value='adults'>
+                    <option value="1"></option>
+                    <option value="2"></option>
+                    <option value="3"></option>
+                    <option value="4"></option>
+                    <option value="5"></option>
+                    <option value="6"></option>
+                </select>
+                <select name="" id="" value='adults'>
+                    <option value="1"></option>
+                    <option value="2"></option>
+                    <option value="3"></option>
+                    <option value="4"></option>
+                    <option value="5"></option>
+                    <option value="6"></option>
+                </select>
+               </div>
+          </div> 
+         
+         <img src={img} alt="image" className={style.carousel}/></div>)}
 
 
 
