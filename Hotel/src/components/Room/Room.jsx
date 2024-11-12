c2b960265dda252ff816c6f9b22f8112d2ba64b9
Temesgen-lab <153854077+Temesgen-lab@users.noreply.github.com>
2024-11-12 21:00:06 +0000

    room css

--- Hotel/src/components/Room/Room.jsx
+++ Hotel/src/components/Room/Room.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import style from './room.module.css';
+import img from './room1.jpg';
+
 function Room() {
     return (
-<div>
-  
+<div className={style.card_container} >
+   <div className={style.card_img_box}>
+        {/* <div className={style.img_content}>
+            <p>type</p>
+            <p>rate</p>
+        </div> */}
+       <div className={style.img_box}>
+        <img src={img}  alt=''className={style.img} />
+       </div>
+   </div>
+   <div className={style.price_box}>
+    <p>price</p>
+    <a href='#' >view details</a>
+   </div>
 </div>
     )
 }
--- Hotel/src/pages/Home/Home.jsx
+++ Hotel/src/pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Header from '../../components/Header/Header';
 import Banner from '../../components/Banner/Banner.jsx';
-
+import Room from '../../components/Room/Room.jsx'
 function Home() {
   return (
     <div>
       <Header />
       <Banner />
+      <Room />
     </div>
   )
 }
