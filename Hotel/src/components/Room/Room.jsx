bdfc75264db8b671e352a7d986783616df4497d6
Temesgen-lab <153854077+Temesgen-lab@users.noreply.github.com>
2024-11-13 00:14:13 +0000

    rooms completed

--- Hotel/src/components/Room/Room.jsx
+++ Hotel/src/components/Room/Room.jsx
@@ -1,25 +1,22 @@
 import React from 'react'
 import style from './room.module.css';
-import img from './room1.jpg';
+import RoomCard from './RoomCard';
+import data from './img/data'
 
 function Room() {
-    return (
-<div className={style.card_container} >
-   <div className={style.card_img_box}>
-        {/* <div className={style.img_content}>
-            <p>type</p>
-            <p>rate</p>
-        </div> */}
-       <div className={style.img_box}>
-        <img src={img}  alt=''className={style.img} />
+    console.log(data)
+    return <>
+    <div className={style.room_outer_container}>
+       <div className={style.room_heading}>
+        <h2 className={style.room_title}>our <span>rooms</span></h2>
+        <p> <span>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</span> </p>
        </div>
-   </div>
-   <div className={style.price_box}>
-    <p>price</p>
-    <a href='#' >view details</a>
-   </div>
-</div>
-    )
+        <div className={style.rooms_container}>
+        {data.map((room,index)=>{
+            return <RoomCard data={room} key={index}/>
+        })}
+        </div></div>
+    </>
 }
 
 export default Room;
