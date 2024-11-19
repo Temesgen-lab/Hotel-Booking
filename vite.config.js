ca825e8857c268ac07cd5e7598addd98894e4790
Temesgen-lab <153854077+Temesgen-lab@users.noreply.github.com>
2024-11-19 10:54:32 +0000

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: '/hotel-booking/',  // Set this to your repo name or subfolder path
   plugins: [react()],
   build: {
-    outDir: 'dist',  // Ensures Vite outputs the build files to the dist folder
-  },
+    outDir: 'dist', // Ensure output is stored in the 'dist' folder
+  }
 })
